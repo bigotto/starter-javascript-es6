d623ca07b579385fa1f4870550b7cd61f13c5604
Bruno Bigotto <bbigotto@gmail.com>
2019-10-07 00:13:11 -0400

    Usando sintaxe de Object short syntax

---
+++ exercicios/07.js
@@ -0,0 +1,15 @@
+const nome = 'Bruno';
+const idade = 24;
+
+// const usuario = {
+//     nome: nome,
+//     idade: idade,
+//     cidade: 'Campo Grande'
+// };
+
+const usuario = {
+    nome,
+    idade,
+    cidade: 'Campo Grande',
+};
+console.log(usuario);
