f461963cf144b7157675974f30457aacd24ae4f6
Bruno Bigotto <bbigotto@gmail.com>
2019-10-07 00:12:22 -0400

    Usando o template literals

---
+++ exercicios/06.js
@@ -0,0 +1,6 @@
+const usuario = 'Bruno';
+const idade = 24;
+
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
+//Template literals
+console.log(`O usuário ${usuario} possui ${idade} anos`);
