ca6ed2d4f91f1b31997f133d5ee6df4e9f328434
Bruno Bigotto <bbigotto@gmail.com>
2019-10-14 23:18:58 -0400

    WebPack server e export default de funções

---
+++ web-pack/src/main.js
@@ -0,0 +1,8 @@
+// import soma, { sub as subFunction } from './funcoes';
+// import * as funcoes from './funcoes'
+// import somaFunction from './soma';
+
+// console.log(funcoes.sub(3,3));
+// console.log(soma(1,2));
+// console.log(subFunction(4,2));
+alert('Ola');
